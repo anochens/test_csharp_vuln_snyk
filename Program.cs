fffc6a6d93f46f55ce59c538695d6f53faee400b
Aron Nochenson <anochenson@gmail.com>
2023-05-30 18:56:58 +0300

    start

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections.Concurrent;
+
+void SayHello(string name)
+{
+    Console.WriteLine($"Hello from thread to {name}");
+}
+
+var i = 0;
+ConcurrentStack<int> mylist = new();
+while(i < 10)
+{
+    var t = new Thread(() => mylist.Push(i));
+    t.Start();
+    // t.Join();
+    i++;
+}
+
+foreach (var j in mylist)
+{
+    Console.WriteLine(j);
+}
+
+
+Thread.Sleep(1);
+Console.WriteLine("Hello from main");
+
+var dangerous = Console.ReadLine();
+
+void dothing()
+{
+    System.Diagnostics.Process.Start(dangerous);
+}
